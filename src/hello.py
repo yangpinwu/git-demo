225b28b54b3cd8ee9c58ede36aa139be4295f604
yangpinwu <yangpinwu@outlook.com>
2026-08-18 22:16:02 +0800

    fix

--- src/hello.py
+++ src/hello.py
@@ -1,9 +1,1 @@
 print("Hello, World!")
-<<<<<<< HEAD
-print("Hello, My World!")
-
-print("This is a local branch")
-=======
-print("This is a local branch")
-print("This is a local branch change2")
->>>>>>> f3157aa (local branch change2)
